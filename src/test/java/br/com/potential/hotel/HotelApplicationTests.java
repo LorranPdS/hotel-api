b5680ad8dba37e70094538d4d47873918f6bda80
LorranPdS <lorransantospereira@yahoo.com.br>
2025-06-03 23:29:46 -0300

    initial project and dependencies

---
+++ src/test/java/br/com/potential/hotel/HotelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.potential.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
