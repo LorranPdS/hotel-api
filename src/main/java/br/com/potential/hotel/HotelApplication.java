9dd23c48d04818c1668ddde470e34df51dbb950b
LorranPdS <lorransantospereira@yahoo.com.br>
2025-06-11 23:31:52 -0300

    feat: setting postgres on application

--- src/main/java/br/com/potential/hotel/HotelApplication.java
+++ src/main/java/br/com/potential/hotel/HotelApplication.java
@@ -9,6 +9,6 @@ public class HotelApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelApplication.class, args);
 	}
-	// TODO: iniciar trabalhos a partir do item 4 do NOTION - https://www.notion.so/lorranpsantos/4-cria-o-do-arquivo-Dockerfile-para-o-projeto-inventory-api-1930bc50a9a680808a0fcd3015b9410c
+	// TODO: iniciar trabalhos a partir do item 5 do NOTION - https://www.notion.so/lorranpsantos/5-configurando-o-PostgreSQL-na-aplica-o-1940bc50a9a680b994c3ffcdb6b2d4f2
 
 }
