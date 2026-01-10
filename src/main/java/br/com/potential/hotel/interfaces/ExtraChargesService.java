5df446b0c44b7add25cf1bfa502d9955be928a15
LorranPdS <lorransantospereira@yahoo.com.br>
2026-01-09 22:32:43 -0300

    feat: created method findAll for extracharges

--- src/main/java/br/com/potential/hotel/interfaces/ExtraChargesService.java
+++ src/main/java/br/com/potential/hotel/interfaces/ExtraChargesService.java
@@ -2,8 +2,12 @@ package br.com.potential.hotel.interfaces;
 
 import br.com.potential.hotel.dto.requests.ExtraChargesRequest;
 import br.com.potential.hotel.dto.responses.ExtraChargesResponse;
+import br.com.potential.hotel.dto.responses.PageResponseDto;
+import org.springframework.data.domain.Pageable;
 
 public interface ExtraChargesService {
+
+    PageResponseDto<ExtraChargesResponse> findAll(Pageable pagination);
 
     ExtraChargesResponse save(ExtraChargesRequest extraChargesRequest);
 
--- src/main/java/br/com/potential/hotel/services/ExtraChargesServiceImpl.java
+++ src/main/java/br/com/potential/hotel/services/ExtraChargesServiceImpl.java
@@ -2,12 +2,14 @@ package br.com.potential.hotel.services;
 
 import br.com.potential.hotel.dto.requests.ExtraChargesRequest;
 import br.com.potential.hotel.dto.responses.ExtraChargesResponse;
+import br.com.potential.hotel.dto.responses.PageResponseDto;
 import br.com.potential.hotel.entity.ExtraChargesEntity;
 import br.com.potential.hotel.interfaces.ExtraChargesService;
 import br.com.potential.hotel.interfaces.ReservationService;
 import br.com.potential.hotel.repositories.ExtraChargesRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -15,16 +17,27 @@ import org.springframework.stereotype.Service;
 public class ExtraChargesServiceImpl implements ExtraChargesService {
 
     @Autowired
-    private ExtraChargesRepository repository;
+    private ExtraChargesRepository extraChargesRepository;
 
     @Autowired
     private ReservationService reservationService;
 
     @Override
+    public PageResponseDto<ExtraChargesResponse> findAll(Pageable pagination) {
+        var categoryPage = extraChargesRepository.findAll(pagination)
+                .map(ExtraChargesResponse::of);
+
+        return new PageResponseDto<>(
+                categoryPage.getContent(),
+                categoryPage.getTotalElements(),
+                categoryPage.getTotalPages());
+    }
+
+    @Override
     public ExtraChargesResponse save(ExtraChargesRequest extraChargesRequest) {
     // sempre lembrar de criar validations (ver se campos foram preenchidos e tal) e lançar as exceptions
         var reservationEntity = reservationService.findByIdEntity(extraChargesRequest.getReservationId());
-        var extraChargesEntity = repository.save(ExtraChargesEntity.of(extraChargesRequest, reservationEntity));
+        var extraChargesEntity = extraChargesRepository.save(ExtraChargesEntity.of(extraChargesRequest, reservationEntity));
         log.info("Extracharge {} saved with sucessfull", extraChargesEntity.getDescription());
 
         return ExtraChargesResponse.of(extraChargesEntity);
